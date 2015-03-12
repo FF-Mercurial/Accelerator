c8cf8a1c4fdb2b003af476b2fbc4c6b9580ef53c
FF-Mercurial <1362832680@qq.com>
2015-03-13 02:04:59 +0800

    add SupportTaskManager

--- app/controller/SupportTaskManager.rb
+++ app/controller/SupportTaskManager.rb
@@ -1,9 +1,12 @@
 require './JsonInputStream'
 require './JsonOutputStream'
+require './Part'
+require './SupportTask'
 
 class SupportTaskManager
     def initialize socket
         @socket = socket
+        @tasks = {}
         @input = JsonInputStream.new @socket do |jsonData|
             inputHandler jsonData
         end
@@ -13,7 +16,49 @@ class SupportTaskManager
         type = jsonData['type']
         case type
         when 'new'
-            url = jsonData[]
+            id = jsonData['id']
+            url = jsonData['url']
+            newTask id, url
+        when 'delete'
+            id = jsonData['id']
+            deleteTask id
+        when 'part'
+            id = jsonData['id']
+            part = jsonData['part']
+            part = Part.new part[0], part[1]
+            pushPart id, part
         end
+    end
+
+    def newTask id, url
+        task = SupportTask.new self, id, url
+        @tasks[id] = task
+    end
+
+    def deleteTask id
+        @tasks[id].delete
+        @tasks.delete id
+    end
+
+    def pushPart id, part
+        @tasks[id].pushPart part
+    end
+
+    def nextPart id
+        jsonData = {
+            'type' => 'nextPart',
+            'id' => id
+        }
+        @output.write jsonData
+    end
+
+    def sendChunk id, part, chunk
+        jsonData = {
+            'type' => 'chunk',
+            'id' => id,
+            'part' => [part.begin, part.end],
+            'chunk' => chunk
+        }
+        @output.write jsonData
     end
 end
